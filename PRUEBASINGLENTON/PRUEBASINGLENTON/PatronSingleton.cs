82dbab9089d953560ff431f4c6758bd2e5689bfe
SantiagoMoreira3C <56278999+SantiagoMoreira3C@users.noreply.github.com>
2020-01-16 14:44:15 -0500

    PruebaSingleton

---
+++ PRUEBASINGLENTON/PRUEBASINGLENTON/PatronSingleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRUEBASINGLENTON
+{
+    class PatronSingleton
+    {
+        //El patron de diseño Singleton  consiste en que la clase solo tenga una unica instancia
+        //Y proporcionar un punto de acceso en general a ella
+        // en este ejercico se plantea:
+        // que  en un vaso se proporcione agua 
+
+        private static PatronSingleton vaso = null;
+        //creamos un metodo de tipo singletonllamado vaso
+        public static PatronSingleton Vaso
+        {
+            get
+            {
+                //preguntaremos si mi vaso esta vacio
+                if (vaso == null)
+                {
+
+                    vaso = new PatronSingleton();
+                }
+                return vaso;
+            }
+        }
+
+        public string Agua = " ";
+
+        //creamos un constructor 
+        public PatronSingleton()
+        // mediante la clase PatronSinglenton podemos proporcionar :
+        {
+            Agua = " Agua de coco ";
+        }
+    }
+
+}
---
+++ PRUEBASINGLENTON/PRUEBASINGLENTON/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PRUEBASINGLENTON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // como se puede observar evitamos estar instanciando varias veces :
+            Console.WriteLine(PatronSingleton.Vaso.Agua);
+
+            PatronSingleton.Vaso.Agua = "agua de coco";
+            Console.WriteLine(PatronSingleton.Vaso.Agua);
+
+            PatronSingleton.Vaso.Agua = "agua de coco";
+            Console.WriteLine(PatronSingleton.Vaso.Agua);
+
+            Console.ReadKey();
+        }
+    }
+}
